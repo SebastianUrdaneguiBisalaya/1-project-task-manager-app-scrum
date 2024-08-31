e99bcdd011794d7ea10c789cd2514de57b3fc1f5
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-08-31 01:19:17 -0500

    add: Create template to generate task's card. Also, create functions to open and close modals for add and update tasks.

--- scripts/functions.js
+++ scripts/functions.js
@@ -14,6 +14,50 @@ export function moveFilterOptionsToMain() {
   }
 }
 
+export function getNameOfCategoryTask(event) {
+  const value = event.target.textContent.trim();
+  console.log(value);
+}
+
+export function toggleModal(modalId, buttonId, closeId, cardText = null) {
+  const modal = document.getElementById(modalId);
+  const button = buttonId ? document.getElementById(buttonId) : null;
+  const closeButton = document.getElementById(closeId);
+
+  if (button) {
+    button.onclick = function () {
+      modal.style.display = 'flex';
+    };
+    if (cardText) {
+      console.log(cardText);
+    }
+  } else {
+    modal.style.display = 'flex';
+    if (cardText) {
+      console.log(cardText);
+    }
+  }
+
+  closeButton.onclick = function () {
+    modal.style.display = 'none';
+  };
+}
+
+export function generateCard(cardsData, nameContainer) {
+  const template = document.getElementById('template__card');
+  const container = document.querySelector(`.table__stateContainer.${nameContainer}`);
+  cardsData.forEach((item, index) => {
+    const clone = template.content.cloneNode(true);
+    clone.querySelector('.card__titleMain').textContent = item.title;
+    clone.querySelector('.card__descriptionDetail').textContent = item.description;
+    clone.querySelector('.card__moreInfoDetail.text__category').textContent = item.type;
+    clone.querySelector('.card__moreInfoDetail.text__dueDate').textContent = item.date;
+    container.appendChild(clone);
+  });
+}
+
+export function moveCardByStatus() {}
+
 export function addDataToLocalStorage() {}
 
 export function deleteDataToLocalStorage() {}
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,15 @@
-import { moveFilterOptionsToMain } from './functions.js';
+import { moveFilterOptionsToMain, getNameOfCategoryTask, toggleModal, generateCard } from './functions.js';
 
+// Code to do responsive mobile the part of filter buttons
+let buttonsToFilter = document.querySelectorAll('.header__filterOption');
+buttonsToFilter.forEach((item, index) => {
+  item.addEventListener('click', getNameOfCategoryTask);
+});
+
+window.addEventListener('resize', moveFilterOptionsToMain);
+window.addEventListener('load', moveFilterOptionsToMain);
+
+// Code to add functionality of Tab Top Navigation
 document.addEventListener('DOMContentLoaded', function () {
   const tabs = document.querySelectorAll('.tab');
   const states = document.querySelectorAll('.table__state');
@@ -13,5 +23,68 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-window.addEventListener('resize', moveFilterOptionsToMain);
-window.addEventListener('load', moveFilterOptionsToMain);
+const cardsData = [
+  {
+    title: 'Aprender Python, R Studio y Machine Learning',
+    description: 'Realizar las tareas que dejaron en el curso de Análisis de Datos.',
+    type: 'Estudio',
+    date: '2024-09-02',
+  },
+  {
+    title: 'Leer sobre Inteligencia Artificial',
+    description: 'Estudiar el nuevo artículo sobre IA en educación.',
+    type: 'Estudio',
+    date: '2024-09-05',
+  },
+  {
+    title: 'Aprender Python, R Studio y Machine Learning',
+    description: 'Realizar las tareas que dejaron en el curso de Análisis de Datos.',
+    type: 'Estudio',
+    date: '2024-09-02',
+  },
+  {
+    title: 'Leer sobre Inteligencia Artificial',
+    description: 'Estudiar el nuevo artículo sobre IA en educación.',
+    type: 'Estudio',
+    date: '2024-09-05',
+  },
+  {
+    title: 'Aprender Python, R Studio y Machine Learning',
+    description: 'Realizar las tareas que dejaron en el curso de Análisis de Datos.',
+    type: 'Estudio',
+    date: '2024-09-02',
+  },
+  {
+    title: 'Leer sobre Inteligencia Artificial',
+    description: 'Estudiar el nuevo artículo sobre IA en educación.',
+    type: 'Estudio',
+    date: '2024-09-05',
+  },
+  {
+    title: 'Aprender Python, R Studio y Machine Learning',
+    description: 'Realizar las tareas que dejaron en el curso de Análisis de Datos.',
+    type: 'Estudio',
+    date: '2024-09-02',
+  },
+];
+// Code to generate cards
+generateCard(cardsData, 'table__toDos');
+generateCard(cardsData, 'table__inProcess');
+
+// Code to add functionality of Modal
+// Modal to Add
+toggleModal('modal__add', 'id__buttonAdd', 'id__modalClose');
+
+// Modal to Update
+let containerCards = document.querySelectorAll('.table__stateContainer');
+containerCards.forEach((item, index) => {
+  item.addEventListener('click', function () {
+    if (event.target.closest('.card')) {
+      const item = event.target.closest('.card');
+      const cardText = {
+        title: item.querySelector('.card__titleMain').textContent,
+      };
+      toggleModal('modal__update', '', 'id__modalUpdateClose', cardText);
+    }
+  });
+});
